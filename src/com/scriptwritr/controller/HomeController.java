b53db6f25a8a8fa1f2c80e38df297604de3aa739
Carl. K <48504139+carlkarama@users.noreply.github.com>
2022-07-02 00:50:52 +1000

    feat: link home controller with view controller

--- src/com/scriptwritr/controller/HomeController.java
+++ src/com/scriptwritr/controller/HomeController.java
@@ -1,6 +1,19 @@
 package com.scriptwritr.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.AnchorPane;
+
 public class HomeController {
 
+    /**
+     * A page that holds the scene
+     */
+    @FXML private TextArea page;
+
+    /**
+     * Main window of application that holds the page
+     */
+    @FXML private AnchorPane window;
 
 }
