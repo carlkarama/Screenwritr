b6a0a84dbc27fe89ed1d1f9c5f9c51cda2eeffad
Carl .K <48504139+carlkarama@users.noreply.github.com>
2023-06-13 22:54:18 +1000

    ref: remove home fxml

--- src/com/scriptwritr/Scriptwritr.java
+++ src/com/scriptwritr/Scriptwritr.java
@@ -23,28 +23,18 @@ public class Scriptwritr extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/scriptwritr/view/Home.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass()
+                .getResource("/com/scriptwritr/view/Home.fxml"));
+
         Parent root = loader.load();
+
         PageController pageController = loader.getController();
+
         Scene homeScene = new Scene(root);
         stage.setTitle("Scriptwritr");
         stage.initStyle(StageStyle.UNDECORATED);
         stage.setScene(homeScene);
         stage.show();
-
-        pageController.sceneTextArea.setOnKeyPressed(keyEvent -> {
-            System.out.println(keyEvent.getCode().toString());
-        });
-
-        root.setOnMousePressed(event -> {
-            xOffset = event.getSceneX();
-            yOffset = event.getSceneY();
-        });
-
-        root.setOnMouseDragged(event -> {
-            stage.setX(event.getScreenX() - xOffset);
-            stage.setY(event.getScreenY() - yOffset);
-        });
 
     }
 }
--- src/com/scriptwritr/controller/PageController.java
+++ src/com/scriptwritr/controller/PageController.java
@@ -1,12 +1,6 @@
 package com.scriptwritr.controller;
 
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextArea;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -16,33 +10,8 @@ import java.util.ResourceBundle;
 
 public class PageController implements Initializable {
 
-    /**
-     * The control that holds the writers text
-     */
-    @FXML public TextArea sceneTextArea;
-
-    /**
-     * The layout that holds the text area
-     */
-    @FXML public VBox sceneVBox;
-
-    /**
-     * The scene that holds the layout
-     */
-    @FXML public AnchorPane sceneAnchorPane;
-
-    /**
-     * The scroll pane to scroll through multiple scenes
-     */
-    @FXML public ScrollPane scrollPaneScene;
-
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
-    }
-
-    public void observeMouseMoved(MouseEvent mouseEvent) {
-        System.out.println(mouseEvent);
     }
 }
